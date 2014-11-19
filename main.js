1d02a03751292d12183a341c0cb1bd4b1aff6b5d
Brett Wise <brettdwise@gmail.com>
2014-11-19 15:40:16 -0600

    Centered everything. Added normalize.css

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
 
-
-var array = ["red", "blue", "yellow"];
-
+var colors = ["red", "blue", "yellow"];
+var numbers = ["-3px", "-4px", "-5px", "-6px"];
 var counter = 0;
 var nextColor;
 
-function  bgchange() {
-    console.log (counter);
-    counter = (counter + 1) % array.length;
-    nextColor = array[counter];
-
+function bgchange() {
+    counter = (counter + 1) % colors.length;
+    nextColor = colors[counter];
     $(".box").css("background-color", nextColor);
-
 }
-
-
     setInterval(bgchange, 10)
 
-
+// function positionChange() {
+// 	counter = (counter + 1) % numbers.length;
+// 	nextPosition = numbers[counter];
+// 	$("h1:after").css("left", nextPosition);
+// }
+// 	setInterval(positionChange, 10)
 });
